0a21536703844cc3a1c9ee79b2636a5eac8fca39
Saeed Nikdel <1991saeed@gmail.com>
2022-04-05 14:46:25 +0330

    config

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const nftmarketaddress = "0xC00ebf866221bB5b5959826dcf84982EcF9EDb12";
-export const nftaddress = "0xeD1c83ee3A991Efe8f6D945707E8251039E2E3ac";
+export const nftmarketaddress = "0x";
+export const nftaddress = "0x";
